19a843e90e525deef9e6f81799296b1ddcdb76aa
JoeC <joe.chiu@customerservice.nsw.gov.au>
2026-07-24 13:17:24 +1000

    init

--- app/app.py
+++ app/app.py
@@ -130,6 +130,6 @@ def chat(req: ChatRequest):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8888, reload=True)
 
 
